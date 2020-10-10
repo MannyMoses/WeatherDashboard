770527202fe8333fe38bb9560cbf275b20388e68
MannyMoses <msm.prof@hotmail.com>
2020-10-10 06:47:29 -0500

    added initial variables

---
+++ script.js
@@ -0,0 +1,7 @@
+let city = $(".city");
+let wind = $(".wind");
+let humidity = $(".humidity");
+let temp = $(".temp");
+
+let searchArr = [];
+let APIKey = "&appid=b9330bfd0b1bf5fe0c849c27df315565";
