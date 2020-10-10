72f4905352cfcf1bd0a7dd2b479957ef787b5d7c
MannyMoses <msm.prof@hotmail.com>
2020-10-10 06:50:56 -0500

    appened variable together

--- script.js
+++ script.js
@@ -34,4 +34,23 @@ $(document).ready(function () {
           } else {
               searchArr.push(response.name)
               localStorage.setItem("cities", JSON.stringify(searchArr));
-          }
+        }
+        
+        let cityName = $(".jumbotron").addClass("city-weather").text(citySearch + " Weather Details  ");
+            let currentDate = moment().format("  MM-DD-YYYY");
+            let windData = $("<p>").text("Wind Speed: " + response.wind.speed).addClass("lead");
+            let humidityData = $("<p>").text("Humidity: " + response.main.humidity + "%").addClass("lead");
+            var iconcode = response.weather[0].icon;
+            var iconurl = "https://openweathermap.org/img/w/" + iconcode + ".png";
+            let weatherImg = $("<img>").attr("src", iconurl);
+            let date = $("<p>").text(moment.unix().format("MMM Do YY")).addClass("lead");
+            $("#five-day").empty();
+            // Convert the temp to fahrenheit
+            let tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        let roundedTemp = Math.floor(tempF);
+        
+        let tempData = $("<p>").text("Temp (K): " + response.main.temp + "°").addClass("lead");
+        let tempDataF = $("<p>").text("Temp (F): " + roundedTemp + "°").addClass("lead");
+        
+        cityName.append(weatherImg, currentDate, windData, humidityData, tempData, tempDataF);
+        $("container").append(cityName);
